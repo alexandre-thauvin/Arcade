38a75609a1eaf8fbf50f999ba559ea70749f6537
Clemenceau Cedric <cedric.clemenceau@epitech.eu>
2017-04-09 22:12:11 +0200

    centi file

---
+++ games/includes/Centi.hpp
@@ -0,0 +1,23 @@
+#ifndef CENTI_HPP_
+# define CENTI_HPP_
+
+#include "Personnage.hpp"
+
+namespace arcade {
+  class Centi : public Personnage {
+  private:
+    Direction	centiDir;
+    Direction	lastDir;
+  public:
+    Centi(std::vector<Vector2u> const&);
+    ~Centi() {}
+    void	removeLastPos();
+    void	addFirstPos(Vector2u );
+    Direction const& getLastDir() const;
+    void	setLastDir(Direction const&);
+    Direction const& getCentiDir() const;
+    void	setCentiDir(Direction const&);
+  };
+}
+
+#endif /* !CENTI_HPP_ */
